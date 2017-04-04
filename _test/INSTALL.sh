f16a798020e0924323e775353037260e8458abee
Niklas Heer <niklas.heer@gmail.com>
2017-04-04 16:29:18 +0200

    added a test case for an installation

--- _test/INSTALL.sh
+++ _test/INSTALL.sh
@@ -1,7 +1,13 @@
 main () {
     # Test global variables
-    echo $pkg_dir
-    echo $pkg_version
+    echo "pkg_src: ${pkg_src}"
+    echo "pkg_dir: ${pkg_dir}"
+    echo "pkg_ver: ${pkg_ver}"
+    echo "pkg_desc: ${pkg_desc}"
+    echo "pkg_name: ${pkg_name}"
+
+    # Install file
+    install -D -m 644 "${pkg_src}/PhpStorm_400x400_Twitter_logo_white.png" "${pkg_dir}/test.png"
 }
 
 main
