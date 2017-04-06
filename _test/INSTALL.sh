e19928d19ab70cddb8c48301359b0ef64e339a2a
Niklas Heer <niklas.heer@gmail.com>
2017-04-06 11:06:25 +0200

    added a sample DESKTOP file

--- _test/INSTALL.sh
+++ _test/INSTALL.sh
@@ -15,8 +15,9 @@ main () {
     install -D -m 644 "${pkg_src}/PhpStorm_400x400_Twitter_logo_white.png" "${pkg_dir}/test.png"
 
     # Install extracted file
-    rsync -rtl "${pkg_src}/hugo/" "${pkg_dir}" --exclude=README.md --exclude=LICENSE.md
-    chmod 755 "${pkg_dir}/hugo_${pkg_ver}_linux_amd64"
+    # rsync -rtl "${pkg_src}/hugo/" "${pkg_dir}" --exclude=README.md --exclude=LICENSE.md
+    # chmod 755 "${pkg_dir}/hugo_${pkg_ver}_linux_amd64"
+    install -D -m 755 "${pkg_src}/hugo_${pkg_ver}_linux_amd64" "${pkg_dir}/hugo"
 }
 
 main
