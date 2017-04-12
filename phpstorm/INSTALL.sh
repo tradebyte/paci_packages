3d04f11ad87693b43d7474dd5a1ba78d1cf2a9a4
Niklas Heer <niklas.heer@gmail.com>
2017-04-12 17:51:31 +0200

    adds support for installing phpstorm

--- phpstorm/INSTALL.sh
+++ phpstorm/INSTALL.sh
@@ -1,5 +1,7 @@
 main () {
-
+    mkdir -p "${pkg_src}/phpstorm"
+    tar xzf "${pkg_src}/PhpStorm-${pkg_ver}.tar.gz" -C "${pkg_src}/phpstorm" --strip-components=1
+    rsync -rtl "${pkg_src}/phpstorm/" "${pkg_dir}"
 }
 
 main
