8e166f60ab17c5655238e64edf048ba347f4f7ca
Niklas Heer <niklas.heer@gmail.com>
2017-06-12 18:40:08 +0200

    update gitkraken to 2.6.0

--- gitkraken/INSTALL.sh
+++ gitkraken/INSTALL.sh
@@ -1,6 +1,6 @@
 main () {
     mkdir -p "${pkg_src}/gitkraken"
-    tar xzf "${pkg_src}/gitkraken-amd64.tar.gz" -C "${pkg_src}/gitkraken" --strip-components=1
+    tar xzf "${pkg_src}/v${pkg_ver}.tar.gz" -C "${pkg_src}/gitkraken" --strip-components=1
     rsync -rtl "${pkg_src}/gitkraken/" "${pkg_dir}"
 }
 
