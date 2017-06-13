2586725e6a001d384382519f7e1d42611279e7d7
Niklas Heer <niklas.heer@gmail.com>
2017-06-13 15:09:25 +0200

    fix filename

--- font-iosevka/INSTALL.sh
+++ font-iosevka/INSTALL.sh
@@ -1,6 +1,6 @@
 main () {
     mkdir -p "$HOME/.local/share/fonts"
-    unzip "${pkg_src}/01-iosevka-${pkg_ver}.zip" -d "$HOME/.local/share/fonts"
+    unzip "${pkg_src}/iosevka-ss08-${pkg_ver}.zip" -d "$HOME/.local/share/fonts"
 
     echo "Clearing font cache..."
     fc-cache -f
