44d4d7ab5c7096a73b6c7feb8d6455746b0b0666
Niklas Heer <niklas.heer@gmail.com>
2017-06-13 13:20:58 +0200

    fix the icon problem

--- gitkraken/INSTALL.sh
+++ gitkraken/INSTALL.sh
@@ -2,6 +2,7 @@ main () {
     mkdir -p "${pkg_src}/gitkraken"
     tar xzf "${pkg_src}/v${pkg_ver}.tar.gz" -C "${pkg_src}/gitkraken" --strip-components=1
     rsync -rtl "${pkg_src}/gitkraken/" "${pkg_dir}"
+    install -D -m 644 "${pkg_src}/gitkraken.png" "${pkg_dir}/gitkraken.png"
 }
 
 main
