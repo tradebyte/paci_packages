8a5cef16d7594613e85c8f1333087e1990dcd3e2
Niklas Heer <niklas.heer@gmail.com>
2017-06-13 14:06:43 +0200

    fix installation error

--- visual-studio-code/INSTALL.sh
+++ visual-studio-code/INSTALL.sh
@@ -1,6 +1,6 @@
 main () {
     mkdir -p "${pkg_src}/vscode"
-    tar xzf "${pkg_src}/code-stable-code_${pkg_ver}-*.tar.gz" -C "${pkg_src}/vscode" --strip-components=1
+    tar xzf "${pkg_src}/stable" -C "${pkg_src}/vscode" --strip-components=1
     rsync -rtl "${pkg_src}/vscode/" "${pkg_dir}"
 }
 
