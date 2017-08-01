2ccd8aeac3c275d7cd5dc7145af99f1cf7599e57
Niklas Heer <me@nheer.io>
2017-08-01 13:04:41 +0200

    fix install bug

--- jetbrains-toolbox/INSTALL.sh
+++ jetbrains-toolbox/INSTALL.sh
@@ -1,7 +1,7 @@
 main () {
     mkdir -p "${pkg_src}/toolbox"
     tar xzf "${pkg_src}/${pkg_name}-${pkg_ver}.tar.gz" -C "${pkg_src}/toolbox" --strip-components=1
-    rsync -rtl "${pkg_src}/phpstorm/" "${pkg_dir}"
+    rsync -rtl "${pkg_src}/toolbox/" "${pkg_dir}"
 
     chmod +x "${pkg_dir}/${pkg_name}"
 
